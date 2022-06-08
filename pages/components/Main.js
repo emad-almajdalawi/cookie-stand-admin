a16d5fe430b7239bae4e1884a740c98ecadb1089
emad-almajdalawi <majdalawi97@yahoo.com>
2022-06-09 00:36:58 +0300

    feat: usestate JSON string

--- pages/components/Main.js
+++ pages/components/Main.js
@@ -1,15 +1,31 @@
 import Form from '../components/Form'
+import { useState } from 'react'
 
 function Main() {
 
+  const [report, setReport] = useState({});
+
+  function formHandle(event) {
+    event.preventDefault();
+    const CreateReport = {
+      location: event.target.location?.value,
+      minCookies: event.target.minCust?.value,
+      maxCookies: event.target.maxCust?.value,
+      avgCookies: event.target.avgCookies?.value,
+    };
+
+    setReport(CreateReport);
+  }
+
   return (
-      <main className = 'py-8 px-10 text-center'>
-        <dev className = 'relative mx-auto my-10 self-center'>
-          <Form/>
-        </dev>
-        <br/>
-        <p>Report table comming soon</p>
-      </main>
+    <main className='py-8 px-10 text-center'>
+      <dev className='relative mx-auto my-10 self-center'>
+        <Form formHandle={formHandle} />
+      </dev>
+      <br />
+      <p>Report table comming soon</p>
+      <p>{JSON.stringify(report)}</p>
+    </main>
   )
 }
 
