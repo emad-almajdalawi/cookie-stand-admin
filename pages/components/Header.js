0e8c7cd7bd8070c1696328bb689cd8d10dc8bb5b
emad-almajdalawi <majdalawi97@yahoo.com>
2022-06-15 14:34:39 +0300

    feat! : add login without functions

--- pages/components/Header.js
+++ pages/components/Header.js
@@ -4,9 +4,15 @@ function Header() {
   return (
     <header className='py-8 px-8 bg-emerald-500 flex justify-between'>
       <h1 className='text-4xl font-bold'>Cookie Stand Admin</h1>
-      <Link href={{ pathname: "components/overview" }}>
-        <botton type="botton" className="bg-emerald-600 justify-self-center self-center shadow hover:bg-emerald-200 focus:shadow-outline focus:outline-none py-2 px-4 rounded">Overview</botton>
-      </Link>
+      <nav className='flex justify-end gap-5'>
+        <Link href={{ pathname: "components/overview" }}>
+          <botton type="botton" className="bg-emerald-600 justify-self-center self-center shadow hover:bg-emerald-200 focus:shadow-outline focus:outline-none py-2 px-4 rounded">Overview</botton>
+        </Link>
+        <Link href={{ pathname: "components/Login" }}>
+          <botton type="botton" className="bg-emerald-600 justify-self-center self-center shadow hover:bg-emerald-200 focus:shadow-outline focus:outline-none py-2 px-4 rounded">Login</botton>
+        </Link>
+      </nav>
+
     </header>
 
   )
